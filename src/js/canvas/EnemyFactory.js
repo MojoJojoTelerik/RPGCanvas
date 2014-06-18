bb43764b28fe8881766e8039960e500c3d2b8a20
Enlightment <galya.bogdanova@yahoo.com>
2014-06-18 12:36:47 +0300

    Refactoring: collision checking for shots and enemies out of Factory classes and optimized

--- src/js/canvas/EnemyFactory.js
+++ src/js/canvas/EnemyFactory.js
@@ -2,62 +2,27 @@
 	this.image = enemyImage;
     this.layer = enemyLayer;
 	
-    var enemies = [];
-	var maxEnemies = 5;
-	var currentEnemiesCount = 0;
-	
-	var maxPositionX = this.layer.getWidth();
-	var maxPositionY = this.layer.getHeight();
+    this.enemies = [];
+    var maxEnemies = 5;
+
+	var minPositionX = 15;
+	var maxPositionX = this.layer.getWidth() - 15;
+	var minPositionY = 15;
+	var maxPositionY = this.layer.getHeight() - 15;
 	
     this.createEnemy = function () {
-		if (currentEnemiesCount < maxEnemies) {
-			currentEnemiesCount++;
-			
-			var initialPositionX = Math.floor(Math.random() * maxPositionX);
-			var initialPositionY = Math.floor(Math.random() * maxPositionY);
-		
-			var newEnemy = new Enemy(this.image, initialPositionX, initialPositionY);
-			
-			this.layer.add(newEnemy.image);
-			
-			enemies.push(newEnemy);
+		if (this.enemies.length < maxEnemies) {
+		    var initialPositionX = Math.floor(Math.random() * (maxPositionX - minPositionX) + minPositionX);
+		    var initialPositionY = Math.floor(Math.random() * (maxPositionY - minPositionY) + minPositionY);
 
+		    console.log(initialPositionX);
+		    console.log(initialPositionY);
+
+			var newEnemy = new Enemy(this.image, initialPositionX, initialPositionY);
+			this.enemies.push(newEnemy);
+
+			this.layer.add(newEnemy.image);
 			this.layer.draw();
 		}
     };
-	
-    this.enemiesUpdate = function (shots, player) {
-        if (shots.length != 0 && enemies.length != 0) {
-            for (var i = 0; i < shots.length; i++) {
-				var currentShot = shots[i];
-				
-				for (var j = 0; j < enemies.length; j++) {
-					var currentEnemy = enemies[j];
-					if (detectCollision(currentShot.image, currentEnemy.image)) {
-						currentShot.isForRemoving = true;
-						currentEnemy.isForRemoving = true;
-						currentEnemy.image.remove();
-						currentEnemiesCount--;
-						player.life += 50;
-					}
-				}
-            }
-        }
-		
-		// TO DO IT CORRECTLY
-		var enemiesIndexesForRemoval = [];
-		
-        for (var key in enemies) {
-            var currentEnemy = enemies[key];
-
-            if (currentEnemy.isForRemoving) {
-                enemiesIndexesForRemoval.push(key);
-            }
-        }
-
-        for (var index in enemiesIndexesForRemoval) {
-            enemies.splice(index, 1);
-            enemiesIndexesForRemoval.splice(index, 1);
-        }
-	}
 }
--- src/js/canvas/collisions.js
+++ src/js/canvas/collisions.js
@@ -1,4 +1,4 @@
-﻿function isPlayerColliding(nextPosition, player, obstacles) {
+﻿var isPlayerColliding = function (nextPosition, player, obstacles) {
     var isColliding = false;
     var isOutOfFrame = false;
     var isCollidingWithTree = false;
@@ -61,6 +61,63 @@
     return isColliding;
 }
 
+var shotsEnemiesColliding = function (shots, enemies, player) {
+    if (shots.length !== 0 && enemies.length !== 0) {
+        var shotsCloning = jQuery.extend(true, {}, shots);
+        var enemiesCloning = jQuery.extend(true, {}, enemies);
+        
+        //console.log(shotsCloning);
+        //console.log(enemiesCloning);
+        console.log("enemies length" + enemies.length);
+
+        var enemiesIndexesForRemoval = {};
+        var shotsIndexesForRemoval = {};
+
+        var currentEnemy;
+        var currentShot;
+
+        for (var i = 0; i < shotsCloning.length; i++) {
+            currentShot = shotsCloning[i];
+            console.log(currentShot);
+
+            for (var j = 0; j < enemiesCloning.length; j++) {
+                currentEnemy = enemiesCloning[j];
+
+                if (detectCollisionShots(currentShot.image.clone(), currentEnemy.image.clone())) {
+                    console.log("collision detected");
+                    shotsIndexesForRemoval[i] = true;
+                    enemiesIndexesForRemoval[j] = true;
+                    player.life += 50;
+                }
+            }
+        }
+
+        var index;
+
+        for (var key in shots) {
+            if (shots[key].isForRemoving) {
+                shotsIndexesForRemoval[key] = true;
+            }
+        }
+
+        for (index in shotsIndexesForRemoval) {
+            var shotForRemoving = shots[index];
+
+            shotForRemoving.animate.stop();
+            shotForRemoving.image.remove();
+
+            shots.splice(index, 1);
+        }
+
+        for (index in enemiesIndexesForRemoval) {
+            var enemyForRemoving = enemies[index];
+
+            enemyForRemoving.image.remove();
+            enemies.splice(index, 1);
+        }
+    }
+}
+
 function detectCollision(rect1, rect2) {
     var status = false;
 
@@ -73,6 +130,31 @@ function detectCollision(rect1, rect2) {
     var rec2BottomY = rect2.getY() + rect2.getHeight();
     var rec2LeftX = rect2.getX();
     var rec2RightX = rect2.getX() + rect2.getWidth();
+
+    if (!(rec1BottomY < rec2TopY ||
+     rec2BottomY < rec1TopY ||
+     rec1LeftX > rec2RightX ||
+     rec2LeftX > rec1RightX)) {
+        status = true;
+    }
+
+    return status;
+}
+
+function detectCollisionShots(rect1, rect2) {
+    var status = false;
+
+    var rec1TopY = rect1.getY();
+    var rec1BottomY = rect1.getY() + rect1.getHeight();
+    var rec1LeftX = rect1.getX();
+    var rec1RightX = rect1.getX() + rect1.getWidth();
+    console.log(rec1TopY, rec1BottomY, rec1LeftX, rec1RightX);
+
+    var rec2TopY = rect2.getY();
+    var rec2BottomY = rect2.getY() + rect2.getHeight();
+    var rec2LeftX = rect2.getX();
+    var rec2RightX = rect2.getX() + rect2.getWidth();
+    console.log(rec2TopY, rec2BottomY, rec2LeftX, rec2RightX);
 
     if (!(rec1BottomY < rec2TopY ||
      rec2BottomY < rec1TopY ||
